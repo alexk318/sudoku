88a9911c0c7083b5ff9ee265744e3927db3313c0
alexk318 <alex20k.x@gmail.com>
2025-10-27 00:03:51 +0500

    Test

--- src/save.c
+++ src/save.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 void save() {
-    char text[] = "HOW ARE YOU?";
+    char text[] = "HOW ARE YOU?\n";
 
-    FILE* f1 = fopen("save.txt", "wb");
+    FILE* f1 = fopen("../saves/save.txt", "w");
 
     size_t w1 = fwrite(text, sizeof(text[0]), sizeof(text) / sizeof(text[0]) - 1, f1);
+    
+    char text2[] = "GOOD";
+    size_t w2 = fwrite(text2, sizeof(text2[0]), sizeof(text2) / sizeof(text2[0]) - 1, f1);
+
     fclose(f1);
 }
